d01e37b592239d6d6d3ec5ebd56dfc23ef8d4a38
mo3g89 <46707248+mo3g89@users.noreply.github.com>
2019-07-12 13:03:08 +0300

    Update kill-workers.sh

--- scripts/kill-workers.sh
+++ scripts/kill-workers.sh
@@ -16,9 +16,11 @@ done
 
 for i in $(cat /root/workers_list);
 do
-   echo "STOP Jmeter worker $i" >> $JM_LOG
-   echo "Kill Java on $i"
-   ssh -o StrictHostKeyChecking=no -n -f root@$i "sh -c '/usr/bin/pkill java; /usr/bin/pkill jmeter;'"
+   iptables -D INPUT -s $i -j ACCEPT
+   iptables -D OUTPUT -d $i -j ACCEPT
+   IP_FOR_ALLOW=$(ip r g $i |awk -F 'src' '{print $2}'|xargs)
+   ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 -o ConnectionAttempts=2 -n -f root@$i "sh -c 'iptables -D INPUT -s $IP_FOR_ALLOW -j ACCEPT;iptables -D OUTPUT -d $IP_FOR_ALLOW -j ACCEPT;/usr/bin/pkill java; /usr/bin/pkill jmeter > /dev/null 2>&1 &'"
+   [ "x$?" == "x0" ] && echo "Remove iptables rules and stop java on $i [OK]" >> $JM_LOG || echo "Remove iptables rules and stop java on $i [FAILED]" >> $JM_LOG
 done
 
 echo "STOP Jmeter [OK]" >> $JM_LOG
